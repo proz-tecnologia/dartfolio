084055a82a0f00799d910f1535e360a3a81615db
Alencar A. Moreira <30672948+iLolz@users.noreply.github.com>
2022-09-08 20:19:34 -0300

    Update lib/dartfolio.dart
    Add documentation
    
    Co-authored-by: Fabio Martinelli <webmaster@zupy.com.br>

--- lib/dartfolio.dart
+++ lib/dartfolio.dart
@@ -3,6 +3,6 @@ import 'dart:io';
 int calculate() {
   int a = int.parse(stdin.readLineSync(encoding: utf8)!);
   int b = int.parse(stdin.readLineSync(encoding: utf8)!);
-
+// Return the user input
   return a * b;
 }
