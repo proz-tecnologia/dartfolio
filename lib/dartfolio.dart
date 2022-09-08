bea35aed917a56d9d4cdb6991e15145adc71affe
Alencar A. Moreira <30672948+iLolz@users.noreply.github.com>
2022-09-08 20:13:06 -0300

    Update lib/dartfolio.dart
    Adding encoding

--- lib/dartfolio.dart
+++ lib/dartfolio.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 int calculate() {
   int a = int.parse(stdin.readLineSync()!);
-  int b = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync(encoding: utf8)!);
 
   return a * b;
 }
