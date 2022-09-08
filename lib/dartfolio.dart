0aeef0e09fde193ab08295aa1997d9bc3282bb9d
Alencar <alencarantonio@hotmail.com.br>
2022-09-08 20:23:26 -0300

    Adding dart:convert library

--- lib/dartfolio.dart
+++ lib/dartfolio.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 
 int calculate() {
   int a = int.parse(stdin.readLineSync(encoding: utf8)!);
   int b = int.parse(stdin.readLineSync(encoding: utf8)!);
-// Return the user input
+
+  // Return the user input
   return a * b;
 }
